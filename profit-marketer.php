e8df356537b8a5e335c1fb345be71b9e9ae64d2b
Ronnel Añasco <ronnel.anasco.vc@gmail.com>
2014-10-07 21:15:29 +0800

    Add latest file updates.

---
+++ profit-marketer.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * GitHub Updater
+ *
+ * @package   ProfitMarketer_Updater
+ * @author    Andy Fragen
+ * @license   GPL-2.0+
+ * @link      https://github.com/afragen/github-updater
+ */
+
+/*
+Plugin Name:       Profit Marketer
+Plugin URI:        https://github.com/afragen/github-updater
+Description:       A plugin to automatically update Profit Marketer plugins and themes
+Version:           2.8.1.13
+Author:            Profit Marketer
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+Domain Path:       /languages
+Text Domain:       profit-marketer
+Profit Marketer Plugin URI: https://github.com/DivaMedia/profit-marketer
+Profit Marketer Branch:     develop
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Load main class
+if ( ! class_exists( 'ProfitMarketer_Updater' ) ) {
+	require_once 'includes/class-github-updater.php';
+	require_once('includes/class-github-api.php');
+	require_once('includes/class-bitbucket-api.php');
+	require_once('includes/class-plugin-updater.php');
+	require_once('includes/class-theme-updater.php');
+}
+
+// Instantiate class ProfitMarketer_Updater
+new ProfitMarketer_Updater;
+
+/**
+ * Calls ProfitMarketer_Updater::init() in init hook so other remote upgrader apps like
+ * InfiniteWP, ManageWP, MainWP, and iThemes Sync will load and use all
+ * of ProfitMarketer_Updater's methods, especially renaming.
+ */
+add_action( 'init', array( 'ProfitMarketer_Updater', 'init' ) );
